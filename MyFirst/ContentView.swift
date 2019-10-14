f902d6826ebecce0bf7a2db474c1c27b065ffa4d
katsuNakajima <katugorou.karma@mineo.jp>
2019-10-14 20:41:06 +0900

    Initial Commit

---
+++ MyFirst/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFirst
+//
+//  Created by 中嶋克聡 on 2019/10/14.
+//  Copyright © 2019 KatsuNakajima. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
